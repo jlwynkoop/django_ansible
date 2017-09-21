31ecabca1087e8f50df66adfafe15e1de99d70f1
John Wynkoop <jlwynkoop@gmail.com>
2017-09-21 00:15:04 -0400

    .

--- run_demo.sh
+++ run_demo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+rm -rf devenv-key.pem
 aws ec2 create-key-pair --region ap-northeast-1 --key-name devenv-key --query 'KeyMaterial' --output text > devenv-key.pem
-chmod 400 devenv-key.pem
+chmod 600 devenv-key.pem
 ansible-playbook -i localhost, -e "group=django-dev" hello_world.yml
 
